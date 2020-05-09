4d16274ac85590d4668a8cd0f95e73afa1cbf7b5
Dragonfood78 <64845435+Dragonfood78@users.noreply.github.com>
2020-05-09 19:05:54 +0300

    flag fgv
    flag gfgv miatt valasztott ki 4et egyszerre ujabb verzio mar jol mukodik, kiveve textboxok es winner

--- widget.cpp
+++ widget.cpp
@@ -1,16 +1,21 @@
 #include "widget.hpp"
 #include "graphics.hpp"
-Widget::Widget(int x,int y,int sx,int sy)
-{
-    xhely=x;
-    yhely=y;
-    XX=sx;
-    YY=sy;
-}
-bool Widget::flag(int pos_x,int pos_y)const{
-    return pos_x>xhely && pos_y>>yhely && pos_x<xhely+XX && pos_y<yhely+YY;
+using namespace genv;
 
+Widget::Widget(int x, int y, int sx, int sy) { //konstruktor
+    _x = x;
+    _y = y;
+    _size_x = sx;
+    _size_y = sy;
 }
 
-void Widget::draw()const{};
-void Widget::handle(genv::event ev){};
+bool Widget::is_selected(int mouse_x, int mouse_y) const { //rajta van e az egér
+    return mouse_x > _x && mouse_x < _x + _size_x && mouse_y >_y && mouse_y < _y +_size_y;
+}
+
+void Widget::draw() const { //widget kirajzolása
+}
+
+void Widget::handle(event ev) { //2idget "kezelése"
+}
+
